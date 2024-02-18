15deb07e0dc68ca78d70b4a1d38fe21efebcadc2
Victor Bogdanow Dornelles <victor.bogdanowdornelles@gmail.com>
2024-02-18 14:25:14 -0300

    feat: product name captalize

--- back-end/src/controller/product/parser.ts
+++ back-end/src/controller/product/parser.ts
@@ -2,7 +2,9 @@ import { Request } from "express";
 import { z } from "zod";
 
 const bodyObject = z.object({
-  name: z.string().min(3),
+  name: z.string().min(3).transform((name) => {
+    return name.charAt(0).toUpperCase() + name.slice(1).toLocaleLowerCase();
+  }),
   description: z.string(),
   price: z.number().min(0).nonnegative("O preço não pode ser negativo.").transform((n) => parseFloat(n.toFixed(2))),
   quantity: z.number().min(0).nonnegative("A quantidade não pode ser negativa."),
