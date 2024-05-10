23d6fc1295d2c378cad8f3ae186bc8eda72b992c
Victor Bogdanow Dornelles <victor.bogdanowdornelles@gmail.com>
2024-05-10 00:52:11 -0300

    Feat: delete products together with list

--- back-end/src/repository/shopping-list/delete/index.ts
+++ back-end/src/repository/shopping-list/delete/index.ts
@@ -2,6 +2,20 @@ import { DeleteShoppingListParams } from "../../../controller/shopping-list/dele
 import { prismaClient } from "../../../lib/prisma/prismaClient";
 
 export async function deleteShoppingListRepository({id}: DeleteShoppingListParams) {
+  const productsToDelete = await prismaClient.product.findMany({
+    where: {
+      shoppingListId: id
+    }
+  });
+
+  await Promise.all(productsToDelete.map(async (product) => {
+    await prismaClient.product.delete({
+      where: {
+        id: product.id
+      }
+    });
+  }));
+
   const response = await prismaClient.shoppingList.delete({
     where: {
       id
